70f8b520976d257ccedade644e066facf5ef0e38
armancse-dev <armanhossaincse7@gmail.com>
2022-06-24 22:58:19 +0600

    Explore News API to integrate react bootstrap Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Spinner, Row, Col, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import News from './components/News/News';
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() =>{
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-05-24&sortBy=publishedAt&apiKey=37b47d819f7048deabe7280236759725')
+    .then(res => res.json())
+    .then(data => setNews(data.articles))
+
+  }, [])
   return (
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row xs={1} md={2} className="g-4">
+        {
+          news.map(nw => <News news={nw}></News>)
+        }
+      </Row>
     </div>
   );
 }
